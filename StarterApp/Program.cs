c2a800aa9b058a11ec37838059e3359a85f88355
MarijaNet <MarijaNet@LAPTOP-CTEM9FAL>
2021-11-01 21:10:28 +0200

    Add project files.

---
+++ StarterApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StarterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, write the text");
+            var text = Console.ReadLine();
+            foreach (var ch in text)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
